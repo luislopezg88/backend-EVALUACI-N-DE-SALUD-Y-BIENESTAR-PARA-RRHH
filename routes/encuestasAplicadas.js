4da8bde81161daa70e4b1e28d65622f67589dcae
christopher-1988 <christopher_carnevale@outlook.com>
2023-11-03 18:12:37 -0500

    fix correccion

--- routes/encuestasAplicadas.js
+++ routes/encuestasAplicadas.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Encuestas = require("../schema/respuesta");
 const Cuestionarios = require("../schema/cuestionario");
+const Empleado = require("../schema/empleado");
 const { jsonResponse } = require("../lib/jsonResponse");
 
 const router = express.Router();
@@ -10,16 +11,18 @@ router.get("/:cuestionarioId", async function (req, res) {
   try {
     const data = await Encuestas.find({
       cuestionario_id: cuestionarioId,
-    })
-      .populate("empleado_id", "name") // Poblar el campo 'empleado_id' y seleccionar solo el campo 'name' del modelo Empleado
-      .exec();
+    });
+
+    const empleados = await Empleado.find({});
 
     res.json(
       jsonResponse(200, {
         data,
+        empleados,
       })
     );
   } catch (err) {
+    console.log(err);
     return res.status(500).json(
       jsonResponse(500, {
         error: "Error al obtener las encuestas aplicadas",
