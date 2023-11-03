22ada0329340c0c34aa5f0dc1a034faf800a976d
Jorge C <jorgecolmenarez89@gmail.com>
2023-11-03 09:57:50 -0400

    administrar tareas

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const moment = require('moment');
 const { jsonResponse } = require("../lib/jsonResponse");
 const log = require("../lib/trace");
 const router = express.Router();
 const Tarea = require("../schema/tarea");
+const tarea = require("../schema/tarea");
+
+const today = moment().startOf('day')
+
 
 router.get("/byUser/:userId", async function (req, res) {
   const userId = req.params.userId;
   try {
-    const perfil = await Tarea.findOne({ user: userId }).populate('user');
+    const tareas = await Tarea.find({ user: userId }).populate('user');
     res.json(
       jsonResponse(200, {
-        perfil,
+        tareas,
       })
     );
   } catch (err) {
@@ -22,17 +27,24 @@ router.get("/byUser/:userId", async function (req, res) {
   }
 });
 
-router.get("/user/:userId/date/:date", async function (req, res) {
+router.get("/today/:userId/date/:date", async function (req, res) {
   const userId = req.params.userId;
-  const data = req.params.date;
+  const date = req.params.date;
   try {
-    const perfil = await Tarea.findOne({ user: userId, created: date }).populate('user');
+    const tareas = await Tarea.find({ 
+      user: userId,
+      created: {
+        $gte: `${date} 00:00:00`,
+        $lte: `${date} 23:59:59`
+      }
+   }).populate('user');
     res.json(
       jsonResponse(200, {
-        perfil,
+        tareas,
       })
     );
   } catch (err) {
+    console.log(err)
     return res.status(500).json(
       jsonResponse(500, {
         error: "Error al obtener la lista de empleados",
@@ -43,28 +55,39 @@ router.get("/user/:userId/date/:date", async function (req, res) {
 
 router.post("/save/:userId", async function (req, res, next) {
   const user = req.params.userId;
-  const { name, description, birthdate, completed } = req.body;
+  const { name, description } = req.body;
   try {
-    const tarea = await Perfil.findOne({ user })
-    if (!tarea) {
-      const newTarea = new Tarea({ name, description, created: new Date(), completed: false, user });
-      await newTarea.save();
-      res.json(
-        jsonResponse(200, {
-          message: "Tarea actualizada satisfactoriamente",
-        })
-      );
-    } else {
-      tarea.name = name;
-      tarea.description = description;
-      tarea.completed = completed;
-      await tarea.save();
-      res.json(
-        jsonResponse(200, {
-          message: "Tarea actualizada satisfactoriamente",
-        })
-      );
-    }
+    const newTarea = new Tarea({ name, description, created: new Date(), completed: false, user });
+    await newTarea.save();
+    res.json(
+      jsonResponse(200, {
+        message: "Tarea creada satisfactoriamente",
+      })
+    );
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json(
+      jsonResponse(500, {
+        error: "Error creando el empleado",
+      })
+    );
+  }
+});
+
+router.put("/update/:id", async function (req, res, next) {
+  const id = req.params.id;
+  const { name, description, completed } = req.body;
+  try {
+    const tarea = Tarea.findById({ _id: id });
+    tarea.name = name;
+    tarea.description = description;
+    tarea.completed = completed;
+    await newTarea.save();
+    res.json(
+      jsonResponse(200, {
+        message: "Tarea actualizada satisfactoriamente",
+      })
+    );
   } catch (err) {
     console.log(err)
     return res.status(500).json(
